6b0a0e19f561f686ef7edd928d013c00001944a9
John Wyles <john@fourstacks.co.uk>
2019-10-21 12:35:48 +0100

    Use form input generator from store

--- src/lib/PolarisFormBuilder.tsx
+++ src/lib/PolarisFormBuilder.tsx
@@ -1,6 +1,5 @@
 import * as React from 'react';
 import { memo, useContext } from 'react';
-import * as fieldInputs from './fields';
 import { IField } from './interfaces/IField';
 import { IStore } from './interfaces/IStore';
 import { FormLayout } from '@shopify/polaris';
@@ -11,18 +10,11 @@ const FormBuilder = (props: IStore) => {
   const store = useContext(StoreContext);
   store.init(props);
 
-  const getFieldName = input => {
-    let ucInput = input.charAt(0).toUpperCase() + input.slice(1);
-
-    return fieldInputs[ucInput + 'Field'];
-  };
-
-  console.log('Rerendering form builder');
   return (
     <StoreContext.Provider value={store}>
       <FormLayout>
         {props.fields.map((field: IField, index: number) => {
-          const Field = getFieldName(field.input);
+          const Field = store.getFieldName(field);
 
           return <Field field={field} key={index} />;
         })}
