9de6309a0712fb453f6d2698c15831e3f49ccff9
John Wyles <john@fourstacks.co.uk>
2020-05-13 18:55:06 +0100

    switch to deep clone in transform fields helper

--- src/lib/helpers/transformFields.ts
+++ src/lib/helpers/transformFields.ts
@@ -1,7 +1,8 @@
 import { FormField } from '../interfaces';
+import _cloneDeep from 'lodash.clonedeep';
 
 export const transformFields = (fields: FormField[], callback: (FormField) => FormField) => {
   return fields.map((field: FormField) => {
-    return { ...callback(field) };
+    return _cloneDeep(callback(field));
   });
 };
