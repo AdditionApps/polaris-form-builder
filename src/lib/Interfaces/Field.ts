c70487ebfd23d9b6f81f60ebcda45230daf0cbbc
John Wyles <john@fourstacks.co.uk>
2020-06-10 15:19:33 +0100

    add repeater-specific keys back into Field interface

--- src/lib/Interfaces/Field.ts
+++ src/lib/Interfaces/Field.ts
@@ -21,4 +21,6 @@ export interface Field {
   emptyMessage?: string;
   layout?: "stacked" | "grouped" | "condensed";
   addButtonText?: string;
+  title?: string;
+  defaultValue: string | number
 }
